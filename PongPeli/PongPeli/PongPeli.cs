66bd7e8520043033e29c79eee55bf781865f1d85
JosseBoxNice <joosua.virtanen08@gmail.com>
2024-11-29 11:01:34 +0200

    Players moving.
    The players are moving now!!

--- PongPeli/PongPeli/PongPeli.cs
+++ PongPeli/PongPeli/PongPeli.cs
@@ -1,5 +1,3 @@
-using System.Runtime.InteropServices;
-using System.Threading;
 using Jypeli;
 
 namespace PongPeli;
@@ -11,25 +9,26 @@ namespace PongPeli;
 /// </summary>
 public class PongPeli : PhysicsGame
 {
+    private readonly Vector nopeusYlos = new Vector(0, 200);
+    private readonly Vector nopeusAlas = new Vector(0, -200);
+
     public override void Begin()
     {
         PhysicsObject pallo = LuoPallo(this, -200, 0);
+        PhysicsObject maila1 = LuoMaila(this,Level.Left + 20.0, 0.0);
+        PhysicsObject maila2 = LuoMaila(this,Level.Right - 20.0, 0.0);
+
 
         LuoKentta();
+        AsetaOhjaimet(maila1, maila2);
         AloitaPeli(pallo);
+        
 
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
 
     private void LuoKentta()
     {
-        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0, Shape.Rectangle);
-        maila.Color = Color.Black;
-        maila.X = Level.Left + 20.0;
-        maila.Y = 0.0;
-        maila.Restitution = 1.0;
-        Add(maila);
-
         Level.Background.Color = Color.Lime;
         Level.CreateBorders(1.0, false);
         
@@ -47,6 +46,38 @@ public class PongPeli : PhysicsGame
         pallo.MomentOfInertia = double.PositiveInfinity;
         peli.Add(pallo);
         return pallo;
+    }
+
+    public static PhysicsObject LuoMaila(PhysicsGame peli, double x, double y)
+    {
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0, Shape.Rectangle);
+        maila.X = x;
+        maila.Y = y;
+        maila.Color = Color.Black;
+        maila.Restitution = 1.0;
+        peli.Add(maila);
+        return maila;
+    }
+
+    private void AsetaOhjaimet(PhysicsObject maila1, PhysicsObject maila2)
+    {
+        Keyboard.Listen(Key.W, ButtonState.Down, AsetaNopeus, "Pelaaja 1: Liiuta mailaa ylös", maila1, nopeusYlos);
+        Keyboard.Listen(Key.W, ButtonState.Released, AsetaNopeus, null, maila1, Vector.Zero);
+        Keyboard.Listen(Key.S, ButtonState.Down, AsetaNopeus, "Pelaaja 1: Liikuta mailaa alas", maila1, nopeusAlas);
+        Keyboard.Listen(Key.S, ButtonState.Released, AsetaNopeus, null, maila1, Vector.Zero);
+
+        Keyboard.Listen(Key.Up, ButtonState.Down, AsetaNopeus, "Pelaaja 2: Liiuta mailaa ylös", maila2, nopeusYlos);
+        Keyboard.Listen(Key.Up, ButtonState.Released, AsetaNopeus, null, maila2, Vector.Zero);
+        Keyboard.Listen(Key.Down, ButtonState.Down, AsetaNopeus, "Pelaaja 2: Liikuta mailaa alas", maila2, nopeusAlas);
+        Keyboard.Listen(Key.Down, ButtonState.Released, AsetaNopeus, null, maila2, Vector.Zero);
+
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
+    }
+
+    public static void AsetaNopeus(PhysicsObject maila, Vector nopeus)
+    {
+        maila.Velocity = nopeus;
     }
 
     private static void AloitaPeli(PhysicsObject pallo)
