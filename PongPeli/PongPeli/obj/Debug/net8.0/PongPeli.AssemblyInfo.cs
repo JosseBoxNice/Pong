ffae0cdac363f25cca8c1b5c87d8daddc65ca909
JosseBoxNice <joosua.virtanen08@gmail.com>
2024-11-30 03:40:31 +0200

    save

--- PongPeli/PongPeli/obj/Debug/net8.0/PongPeli.AssemblyInfo.cs
+++ PongPeli/PongPeli/obj/Debug/net8.0/PongPeli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PongPeli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481bd840f6849e9d66bd6d08429e40dd683bee8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66bd7e8520043033e29c79eee55bf781865f1d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("PongPeli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PongPeli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
